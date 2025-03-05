b6cd559912ba8c029bac7548893da346b96d8528
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-05 15:25:54 +0200

    Update fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -1,4 +1,5 @@
 fetch-data.js doesn't contain: ["async", "fetchUserData", "function", "apiUrl", "https://jsonplaceholder.typicode.com/users"]
 fetch-data.js doesn't contain: ["dataContainer", "getElementById", "api-data"]
 fetch-data.js doesn't contain: ["try", "catch", "await", "response.json()"]
+fetch-data.js doesn't contain: ["dataContainer.innerHTML"]
 

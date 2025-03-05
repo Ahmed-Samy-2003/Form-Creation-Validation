a197c4ca9d4a45485662924b3325a539d512369e
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-05 15:20:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,4 @@ script.js doesn't contain: ["document.getElementById", "registration-form"]
 script.js doesn't contain: ["form-feedback"]
 script.js doesn't contain: ["username", "email", "password", "trim"]
 script.js doesn't contain: ["push"] 
-script.js doesn't contain: ["display"]
-script.js doesn't contain: ["block"]
-
-
-
-
-Make feedbackDiv visible by setting its style.display to “block”.
-If isValid remains true, set the textContent of feedbackDiv to “Registration successful!” and its style.color to “#28a745”.
-If isValid is false, join messages with <br> to form a single string, and assign this to theinnerHTML of feedbackDiv. Set feedbackDiv.style.color to "#dc3545".
+script.js doesn't contain: ["feedbackDiv.innerHTML", "messages.join"]

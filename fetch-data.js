4742f0270a00d8d2411e9fabb3aff8b18ad0562f
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-05 15:26:58 +0200

    Update fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -4,4 +4,5 @@ fetch-data.js doesn't contain: ["try", "catch", "await", "response.json()"]
 fetch-data.js doesn't contain: ["dataContainer.innerHTML"]
 fetch-data.js doesn't contain: ["appendChild", "userList", "document.createElement", "ul"]
 fetch-data.js doesn't contain: ["Failed to load user data"]
+fetch-data.js doesn't contain: ["document.addEventListener", "DOMContentLoaded"]
 

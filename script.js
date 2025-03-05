2c2a0511ce8aadc45ef4b030f3b00a214a2a8ab9
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-05 15:15:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 script.js doesn't contain: ["addEventListener", "DOMContentLoaded", "function"]
-script.js doesn't contain: ["document.getElementById", "registration-form"]  
+script.js doesn't contain: ["document.getElementById", "registration-form"] 
+script.js doesn't contain: ["form-feedback"]
+
 Make feedbackDiv visible by setting its style.display to “block”.
 If isValid remains true, set the textContent of feedbackDiv to “Registration successful!” and its style.color to “#28a745”.
 If isValid is false, join messages with <br> to form a single string, and assign this to theinnerHTML of feedbackDiv. Set feedbackDiv.style.color to "#dc3545".
